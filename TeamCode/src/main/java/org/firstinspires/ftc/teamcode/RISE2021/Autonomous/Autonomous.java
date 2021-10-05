b89929aaeecc1e36b04266f9ab6be1e33ddb7454
Icy Hot <44911827+PhantomofDeath99@users.noreply.github.com>
2021-10-05 11:55:00 -0400

    This creates java files for Teleop and Autonomous. The repository has been renamed.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RISE2021/Autonomous/Autonomous.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.RISE2021.Autonomous;
+
+public class Autonomous {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RISE2021/Teleop/Teleop.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.RISE2021.Teleop;
+
+public class Teleop {
+}
